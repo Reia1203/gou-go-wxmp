11e9fd9f375336f4a038152d075661f7d8c4a67a
Liya Tang <tangliya251@sohu.com>
2022-08-04 19:42:21 +0800

    test

--- pages/review/review.js
+++ pages/review/review.js
@@ -121,5 +121,61 @@ attached: function(){
     }
   },
 
+  submit(e) {
+    console.log(e);
+    const url = app.globalData.baseUrl;
+    let page = this;
+    const id = e.currentTarget.dataset.id;
+    const d = this.data
+    let data = e.detail.value
+    // multiindex:  [1, 0] 
+    // data.categories = d.multiIndex
+    // multiArray = [restaurant bars], [american chinese ...]
+    data.category = d.multiArray[0][d.multiIndex[0]]
+    // subCategory = [american, buffet, chines...] , [all bars], [all cafes]
+    data.sub_category = d.subCategoryArray[d.multiIndex[0]][d.multiIndex[1]]
+    console.log(1111, data)
+    getApp().globalData.spaces.push(data);
+
+    let header = wx.getStorageSync('header')
+    wx.request({
+      
+      url: `${app.globalData.baseUrl}/reviews`,
+      method:'POST',
+      header,
+      data: data,
+      success(res){
+        console.log(res)
+        wx.uploadFile({
+          url: `${url}/spaces/${res.data.review.id}/upload`,
+          filePath: page.data.image_url[0],
+          name: 'file',
+          header: header,
+          success(res) {
+            console.log('this is for upload file', res)
+            wx.showModal({
+              showCancel: false,
+              title: 'Thank you',
+              content: 'Your submission is under review.',
+              success(res) {
+                wx.switchTab({
+                  url: `../landing/landing`,
+                })
+              }
+            })
+          },
+          fail(err) {
+            console
+            console.log(err)
+          }
+        })
+        
+     }
+   })
+
+
+
+  },
+
 
 })
